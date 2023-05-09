11f3f16ce1a77345b9b215acaaa570bc4f69ff7f
jeremie-arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-05-09 16:12:28 +0200

    Chore (front-end): Add copyrights in footer

--- src/views/layouts/footer.blade.php
+++ src/views/layouts/footer.blade.php
@@ -116,15 +116,15 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
-                <p class="rbt-link-hover text-center text-lg-start">Copyright © 2023 Rainbow-Themes. All Rights Reserved</p>
+                <p class="rbt-link-hover text-center text-lg-start">Copyright © 2023 <a href="{{ url('home') }}">
+                        {{ $appName }}</a>. Tous droits
+                    réservés</p>
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
-                <ul class="copyright-link rbt-link-hover justify-content-center justify-content-lg-end mt_sm--10 mt_md--10">
-                    <li><a href="#">Terms of service</a></li>
-                    <li><a href="privacy-policy.html">Privacy policy</a></li>
-                    <li><a href="subscription.html">Subscription</a></li>
-                    <li><a href="{{ url('auth.login_view') }}">Login & Register</a></li>
-                </ul>
+                <p class="copyright-link rbt-link-hover justify-content-center justify-content-lg-end mt_sm--10
+                mt_md--10">Crée par &nbsp;<a href="https://github.com/jeremie-arcidiacono" target="_blank">Jérémie
+                        Arcidiacono</a>
+                </p>
             </div>
         </div>
     </div>
